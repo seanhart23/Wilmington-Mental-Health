d39aa845fe306f7499fb39f5d51ad4c3b8439c18
Sean Hart <31716085+seanhart23@users.noreply.github.com>
2020-11-27 22:32:30 -0500

    Add new carousel

--- public/js/index.js
+++ public/js/index.js
@@ -49,3 +49,46 @@ $(window).scroll(function(){
   $('.nav-link').toggleClass('nav-link-scrolled', $(this).scrollTop() > 100);
 });
 
+/*** Carousel ***/
+
+$(document).ready(function () {
+    $('.slider').slick({
+        dots: false,
+        infinite: true,
+        speed: 10000,
+        cssEase: 'linear',
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        autoplay: true,
+        autoplaySpeed: 0,
+        variableWidth: true,
+        responsive: [
+          {
+            breakpoint: 1400,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+    });
+});
+
+
+
